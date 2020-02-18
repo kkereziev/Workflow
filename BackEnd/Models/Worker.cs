8a1cb0c1167dd6aa9f1fd38dfde264cbec754f57
KrumK <kkereziev@abv.bg>
2020-02-18 11:53:02 +0200

    Working...

---
+++ BackEnd/Models/Worker.cs
@@ -0,0 +1,19 @@
+namespace BackEnd.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Worker
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(400)]
+        public string Bio { get; set; }
+
+        [StringLength(1000)]
+        public virtual string WebPage { get; set; }
+    }
+}
